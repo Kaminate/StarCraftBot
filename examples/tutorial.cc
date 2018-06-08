eec0bebb5952c74738421442ae38270b1dce6b2f
Guilherme C. T <gctrindade@outlook.com>
2018-06-08 01:28:06 -0700

    Copied base...

---
+++ examples/tutorial.cc
@@ -0,0 +1,27 @@
+#include <sc2api/sc2_api.h> // Events, Coordinator
+#include <iostream>
+
+using namespace sc2;
+
+class Bot : public Agent {
+public:
+    virtual void OnGameStart() final {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Coordinator coordinator;
+    coordinator.LoadSettings(argc, argv);
+
+    Bot bot;
+    coordinator.SetParticipants({
+        CreateParticipant(Race::Terran, &bot),
+        CreateComputer(Race::Zerg)
+    });
+
+    coordinator.LaunchStarcraft();
+    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+
+    return 0;
+}
